659d25ed279bcecf00746ac03e1c4e5843b4f718
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-06-18 00:52:43 +0900

    [Feat/#22] 성적표 저장 API 연결

--- src/components/main/buttons/UploadBtn.js
+++ src/components/main/buttons/UploadBtn.js
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { UploadIcon } from "../../../assets/main";
 import "./UploadBtn.css";
 
-function UploadBtn() {
+function UploadBtn({ accessToken, setHasGradCard }) {
   const [selectedFile, setSelectedFile] = useState(null);
   const [isUploading, setIsUploading] = useState(false);
 
@@ -10,6 +10,32 @@ function UploadBtn() {
     setSelectedFile(event.target.files[0]);
     setIsUploading(true);
   };
+
+  useEffect(() => {
+    if (selectedFile) {
+      fetch('http://13.214.147.170:8080/api/v1/grade_card', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': `Bearer ${accessToken}`,
+        },
+        body: JSON.stringify({
+          gradeCard: selectedFile,
+        })
+      })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setHasGradCard(true);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+    }
+    else {
+      console.log('파일을 선택해주세요.');
+    }
+  }, [selectedFile]);
 
   return (
     <div>
