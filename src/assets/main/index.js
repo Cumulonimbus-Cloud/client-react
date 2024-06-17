8f955689d53587db084c2a248d269c4f38816a81
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-06-17 14:02:19 +0900

    [Chore/#8] 카카오 로고 아이콘 추가

--- src/assets/main/index.js
+++ src/assets/main/index.js
@@ -1,2 +1,3 @@
 export { ReactComponent as TranscriptIcon } from './icon_transcript.svg';
 export { ReactComponent as UploadIcon } from './icon_upload.svg';
+export { ReactComponent as KakaoIcon } from './icon_kakao_logo.svg';
