b45c05edeb3f953d977862eb7a24e761fba104cc
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-06-18 01:39:04 +0900

    [Feat/#24] pdf 수정 파일 업로드 기능

--- src/components/main/buttons/EditBtn.js
+++ src/components/main/buttons/EditBtn.js
@@ -1,14 +1,34 @@
+import { useState } from "react";
 import { UploadIcon } from "../../../assets/main";
 import './EditBtn.css';
 
-function EditBtn({ onClick }) {
+function EditBtn() {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+  }
+
   return (
-    <button id="edit-btn">
-      <div id="edit-btn-contents">
-        <UploadIcon id="edit-btn-icon" />
-        <div id="edit-btn-title">성적표 수정</div>
-      </div>
-    </button>
+    <div id="edit-btn-wrapper">
+      <input 
+        type="file" 
+        accept="application/pdf" 
+        onChange={handleFileChange} 
+        style={{ display: 'none' }} 
+        id="pdf-upload-input" 
+      />
+      <button
+        id="edit-btn"
+        onClick={() => document.getElementById('pdf-upload-input').click()}
+      >
+        <div id="edit-btn-contents">
+          <UploadIcon id="edit-btn-icon" />
+          <div id="edit-btn-title">성적표 수정</div>
+        </div>
+      </button>
+    </div>
+    
   );
 }
 
