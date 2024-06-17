a5ac2af5ffaaae372a87bc1bc62bc865204f8fe9
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-06-18 02:51:18 +0900

    [Feat/#26] 채팅 링크 설정

--- src/components/main/buttons/ChattingBtn.js
+++ src/components/main/buttons/ChattingBtn.js
@@ -1,16 +1,17 @@
+import { Link } from "react-router-dom";
 import { ChattingIcon } from "../../../assets/main";
 import './ChattingBtn.css';
 
 function ChattingBtn() {
   return (
-    <div id="chatting-btn-wrapper">
+    <Link id="chatting-btn-wrapper" to="/chat">
       <button id="chatting-btn">
         <div id="chatting-btn-contents">
           <ChattingIcon id="chatting-btn-icon" />
           <div id="chatting-btn-title">채팅하기</div>
         </div>
       </button>
-    </div>
+    </Link>
   );
 }
 
