f0da21c4254df8272b71e3386e1de882d9920150
CHO EUN JUNG <0516dmswjd@gmail.com>
2024-06-18 00:49:47 +0900

    [Feat/#22] pdf 파일 업로드 기능

--- src/components/main/buttons/UploadBtn.js
+++ src/components/main/buttons/UploadBtn.js
@@ -1,14 +1,37 @@
+import React, { useState } from 'react';
 import { UploadIcon } from "../../../assets/main";
 import "./UploadBtn.css";
 
 function UploadBtn() {
+  const [selectedFile, setSelectedFile] = useState(null);
+  const [isUploading, setIsUploading] = useState(false);
+
+  const handleFileChange = (event) => {
+    setSelectedFile(event.target.files[0]);
+    setIsUploading(true);
+  };
+
   return (
-    <button id="upload-btn">
-      <div id="upload-btn-contents">
-        <UploadIcon id="upload-btn-icon" />
-        <div id="upload-btn-title">참고용 성적표 업로드</div>
-      </div>
-    </button>
+    <div>
+      <input 
+        type="file" 
+        accept="application/pdf" 
+        onChange={handleFileChange} 
+        style={{ display: 'none' }} 
+        id="pdf-upload-input" 
+        disabled={isUploading}
+      />
+      <button 
+        id="upload-btn" 
+        onClick={() => document.getElementById('pdf-upload-input').click()}
+        disabled={isUploading}
+      >
+        <div id="upload-btn-contents">
+          <UploadIcon id="upload-btn-icon" />
+          <div id="upload-btn-title">참고용 성적표 업로드</div>
+        </div>
+      </button>
+    </div>
   );
 }
 
